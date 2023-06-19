9892107f197e21ad02637a4d222f02a024dc8b9b
Tolga <130807536+tatmaca77@users.noreply.github.com>
2023-06-20 00:57:04 +0200

    20/06/23 Actions_IFrame

--- src/test/java/techproed/day14_Actions_Faker/Hausaufgabe02.java
+++ src/test/java/techproed/day14_Actions_Faker/Hausaufgabe02.java
@@ -1,5 +1,11 @@
 package techproed.day14_Actions_Faker;
 
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 import techproed.utilities.TestBase;
 
 public class Hausaufgabe02 extends TestBase {
@@ -13,6 +19,28 @@ public class Hausaufgabe02 extends TestBase {
 5.videoyu calistirdiginizi test edin
      */
 
+    @Test
+    public void keyboardActions() {
+        // 2.https://html.com/tags/iframe/ sayfasina gidelim
+        driver.navigate().to("https://html.com/tags/iframe/");
+
+        Actions actions = new Actions(driver);
+
+        // 3.video’yu gorecek kadar asagi inin
+        actions.sendKeys(Keys.PAGE_DOWN,Keys.PAGE_DOWN).perform();
 
 
+        //4.videoyu izlemek icin Play tusuna basin
+        WebElement iframe = driver.findElement(By.xpath("//iframe[1]"));
+        driver.switchTo().frame(iframe);
+        bekle(2);
+        driver.findElement(By.xpath("//button[@class='ytp-large-play-button ytp-button ytp-large-play-button-red-bg']")).click();
+        bekle(4);
+
+        // 5.videoyu calistirdiginizi test edin
+        driver.findElement(By.xpath("//video[@class='video-stream html5-main-video']")).click();
+        WebElement youtube = driver.findElement(By.xpath("//a[@class='ytp-youtube-button ytp-button yt-uix-sessionlink']"));
+        Assert.assertTrue(youtube.isDisplayed());
+        bekle(2);
+    }
 }
