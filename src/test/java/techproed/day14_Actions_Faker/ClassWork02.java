3c24e2e7e0ee5b8381f9be74986c51181cae85b6
Tolga <130807536+tatmaca77@users.noreply.github.com>
2023-06-19 02:42:01 +0200

    19/06/23 Actions_DragDrop

---
+++ src/test/java/techproed/day14_Actions_Faker/ClassWork02.java
@@ -0,0 +1,26 @@
+package techproed.day14_Actions_Faker;
+
+import org.junit.Test;
+import techproed.utilities.TestBase;
+
+public class ClassWork02 extends TestBase {
+
+
+    /**
+    // https://www.jqueryscript.net/demo/jQuery-Plugin-For-Responsive-Flexible-Iframes-Flexy/ sayfasına gidiniz
+    // Videoyu görecek kadar asagiya ininiz
+    // Videoyu izlemek icin Play tusuna basiniz
+    // videoyu durdurunuz
+    // videoyu tam ekran yapınız
+    // videoyu calıstırınız
+    // videoyu kucultunuz
+    // videoyu durdurunuz
+    // Videoyu calistirdiginizi test ediniz
+    // 'jQuery Flexy Plugin Demos' yazısının gorunur oldugunu test ediniz
+     */
+
+    @Test
+    public void test01() {
+        driver.get("https://www.jqueryscript.net/demo/jQuery-Plugin-For-Responsive-Flexible-Iframes-Flexy/");
+    }
+}
