1f4437c9a3fcc75c46fe84612675582a53036494
Tolga <130807536+tatmaca77@users.noreply.github.com>
2023-06-19 12:45:54 +0200

    19/06/23 FileExist, FileDownload und FileUpload

---
+++ src/test/java/techproed/day15_FileExist/C03_FileUpload.java
@@ -0,0 +1,51 @@
+package techproed.day15_FileExist;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import techproed.utilities.TestBase;
+
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class C03_FileUpload extends TestBase {
+
+    /**
+    // masaustunde bir deneme dosyası olusturun
+// 'https://the-internet.herokuapp.com/upload' adresine gidiniz
+// 'Dosya Seç' kısmına deneme dosyasını yükleyiniz
+// 'Upload' butonuna basın
+// 'File Uploaded!' yazısının goruntulendigini test edin
+     */
+
+    @Test
+    public void test01() {
+        // 'https://the-internet.herokuapp.com/upload' adresine gidiniz
+        driver.get("https://the-internet.herokuapp.com/upload");
+
+        // 'Dosya Seç' kısmına deneme dosyasını yükleyiniz
+        WebElement dateiAuswählen = driver.findElement(By.xpath("//input[@id='file-upload']"));
+        bekle(2);
+
+        //dateiAuswählen.sendKeys("C:\\Users\\User\\Desktop\\deneme.txt");
+        /** Bunu dinamik olarak yapmaliyim. */
+
+        String farkliKisim = System.getProperty("user.home");
+        System.out.println("Farkli Kisim : " + farkliKisim);
+        String ortakKisim ="\\Desktop\\deneme.txt";
+
+        String dosyaYolu = farkliKisim + ortakKisim;
+        dateiAuswählen.sendKeys(dosyaYolu);
+        bekle(4);
+
+        // 'Upload' butonuna basın
+        WebElement button = driver.findElement(By.xpath("//input[@id='file-submit']"));
+        button.click();
+        bekle(2);
+
+        // 'File Uploaded!' yazısının goruntulendigini test edin
+        WebElement message = driver.findElement(By.xpath("//h3[text()='File Uploaded!']"));
+        Assert.assertTrue(message.isDisplayed());
+    }
+}
