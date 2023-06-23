ad6fce374130422caa0e47d7765f868feb6909f7
Tolga <130807536+tatmaca77@users.noreply.github.com>
2023-06-23 22:52:36 +0200

    23/06/23 ScreenShot

---
+++ src/test/java/techproed/day18_ScreenShot/C02_TumSayfaResmi.java
@@ -0,0 +1,32 @@
+package techproed.day18_ScreenShot;
+
+import techproed.utilities.TestBase;
+import org.apache.commons.io.FileUtils;
+import org.junit.Test;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import techproed.utilities.TestBase;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class C02_TumSayfaResmi extends TestBase {
+
+    @Test
+    public void test01() throws IOException {
+        //amazon sayfasına gidelim
+        driver.get("https://amazon.com");
+
+        bekle(2);
+
+        //Ve ekran görüntüsünü alalım
+
+        String tarih = new SimpleDateFormat("_hh_mm_ss_ddMMyyyy").format(new Date());
+        String dosyaYolu = "src/test/java/techproed/TumSayfaResmi/screenShot" + tarih + ".jpeg";
+        TakesScreenshot ts = (TakesScreenshot) driver;
+        Files.write(Paths.get(dosyaYolu),ts.getScreenshotAs(OutputType.BYTES));
+    }
+}
