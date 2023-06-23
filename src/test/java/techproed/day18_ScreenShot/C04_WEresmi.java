775c2a36b6f7c0c492359f0cbed7acc6148dce86
Tolga <130807536+tatmaca77@users.noreply.github.com>
2023-06-23 22:55:19 +0200

    23/06/23 ScreenShot

---
+++ src/test/java/techproed/day18_ScreenShot/C04_WEresmi.java
@@ -0,0 +1,35 @@
+package techproed.day18_ScreenShot;
+
+import techproed.utilities.TestBase;
+import org.apache.commons.io.FileUtils;
+import org.junit.Test;
+import org.openqa.selenium.*;
+import techproed.utilities.TestBase;
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class C04_WEresmi extends TestBase {
+
+    /*
+     Bir webelementin resmini almak için önce Webelementi locate edip bir webelemente assing ederiz.
+    ve bu webelementi direk getScreenShotAs() methodunu kullanarak resmini belirttimiz dosyaya kaydederiz.
+     */
+    @Test
+    public void name() throws IOException {
+        //amazon sayfasına gidelim
+        driver.get("https://amazon.com");
+
+        bekle(2);
+        //iphone aratalım
+        driver.findElement(By.id("twotabsearchtextbox")).sendKeys("iphone", Keys.ENTER);
+
+        //çıkan sonuc yazısının resmini alalım
+        WebElement sonucYazisi = driver.findElement(By.xpath("(//*[@class='sg-col-inner'])[1]"));
+        String tarih = new SimpleDateFormat("_hh_mm_ss_ddMMyyyy").format(new Date());
+        String dosyaYolu = "src/test/java/techproed/ElementResmi/WEscreenShot" + tarih + ".jpeg";
+        FileUtils.copyFile(sonucYazisi.getScreenshotAs(OutputType.FILE),new File(dosyaYolu));
+
+    }
+}
