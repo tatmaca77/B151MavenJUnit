eb85d16c62accfedf01c4c387680d295f569c8af
Tolga <130807536+tatmaca77@users.noreply.github.com>
2023-06-23 22:54:07 +0200

    23/06/23 ScreenShot

---
+++ src/test/java/techproed/day18_ScreenShot/C03_TumSayfaResmi.java
@@ -0,0 +1,30 @@
+package techproed.day18_ScreenShot;
+
+import techproed.utilities.TestBase;
+import org.apache.commons.io.FileUtils;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import techproed.utilities.TestBase;
+
+import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class C03_TumSayfaResmi extends TestBase {
+
+    @Test
+    public void test01() {
+        //Techproeducation sayfasına gidelim
+        driver.get("https://techproeducation.com");
+
+        bekle(2);
+        //Çıkan reklamı kapatalım
+        driver.findElement(By.xpath("//i[@class='eicon-close']")).click();
+
+        //Ve ekran görüntüsünü alalım
+
+    }
+
+}
