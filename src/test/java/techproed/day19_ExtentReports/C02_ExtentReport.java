176d3b2203fe2c63bbb459217df9e28c4315659a
Tolga <130807536+tatmaca77@users.noreply.github.com>
2023-06-25 23:36:55 +0200

    25/06/23 ExtentReports

---
+++ src/test/java/techproed/day19_ExtentReports/C02_ExtentReport.java
@@ -0,0 +1,17 @@
+package techproed.day19_ExtentReports;
+
+import techproed.utilities.TestBase;
+import com.aventstack.extentreports.ExtentReports;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import techproed.utilities.TestBase;
+
+public class C02_ExtentReport extends TestBase {
+
+    @Test
+    public void test01() {
+
+
+    }
+}
