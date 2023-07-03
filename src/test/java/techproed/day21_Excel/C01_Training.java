cd1103489e1474596f20b242f10d57bd128210a0
Tolga <130807536+tatmaca77@users.noreply.github.com>
2023-07-03 14:00:37 +0200

    03/07/2023 JSExecutorClick

--- src/test/java/techproed/day21_Excel/C01_Training.java
+++ src/test/java/techproed/day21_Excel/C01_Training.java
@@ -8,6 +8,8 @@ import org.junit.Test;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 public class C01_Training {
 
@@ -40,8 +42,39 @@ public class C01_Training {
         int lastRowNum = workbook.getSheet("Sheet1").getLastRowNum();
         System.out.println("Last Row Number :" + (lastRowNum+1));
 
-        // Kullanilan Satir Saysini bulalim
+        // Kullanilan Satir Sayisini bulalim
         int useToRowNum = workbook.getSheet("Sheet1").getPhysicalNumberOfRows();
         System.out.println("Used to Row Number :" + useToRowNum);
+    }
+
+    @Test
+    public void test03() throws IOException {
+        String filePath = "src/test/java/resources/Capitals.xlsx";
+        FileInputStream fis = new FileInputStream(filePath);
+        Workbook workbook = WorkbookFactory.create(fis);
+
+        /**
+         Tüm Verileri almak istersek; "MAP" en uygun JAVA objesidir. Cünkü Key-Value iliskisi vardir.
+         Bu sebeple ilk sütun Key olur 2.Sütun Value olur.
+         */
+
+        //Capital Dosyasindaki tüm verileri yazdiriniz. !!!!!!
+        Map<String,String> capitalsMap = new HashMap<>();
+
+
+        // En Son olan veriye kadar gitmesi icin son satirin sayisini bulmaliyiz.
+        int lastRowNumber = workbook.getSheet("Sheet1").getLastRowNum();
+        System.out.println("Last Row Number : " + lastRowNumber);
+
+
+        // For i loop acmaliyim her gelen veriyi Key-Value iliskisi ile Map icine atmak icin.
+        for (int i = 0; i < lastRowNumber; i++) {
+            String key = workbook.getSheet("Sheet1").getRow(i).getCell(0).toString();
+            String value = workbook.getSheet("Sheet1").getRow(i).getCell(1).toString();
+
+            capitalsMap.put(key,value);
+        }
+        System.out.println("All the Data of Capitals : " + capitalsMap);
+
     }
 }
